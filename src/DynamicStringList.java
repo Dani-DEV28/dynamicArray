10466745fe53c37e538f94c7d34ad05a498c8ca3
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-04-08 11:59:34 -0700

    started override

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,3 +1,20 @@
 public class DynamicStringList implements StringList{
-    
+    @Override
+    public String get(int index){
+        return "null";
+    }
+
+    @Override
+    public void set(int index, String value){
+
+    }
+
+    @Override
+    public void add(String value){
+        
+    }
+
+    @Override
+    @Override
+    @Override
 }
