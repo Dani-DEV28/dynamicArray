f20db60930e39a679ad2db8e5f1c4d190e3519de
Nur Mohamed <mohamed.nur@student.greenriver.edu>
2025-04-08 12:47:33 -0700

    Update String Remove

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -10,14 +10,29 @@ public class DynamicStringList implements StringList{
     }
 
     @Override
-    public void add(String value){
+    public String remove(int index) {
+        // Check if the index is valid 
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size());
+        }
         
+        // Get the element at the specified index to return later
+        String removedElement = get(index);
+        
+        // Shift all elements after the index one position to the left
+        for (int i = index; i < size() - 1; i++) {
+            set(i, get(i + 1));
+        }
+        
+        // Decrease the size of the list by 1
+        int size;
+        size--;
+        
+        // Return the element that was removed
+        return removedElement;
     }
 
-    @Override
-    public String remove(int index) {
-        
-    }
+   
     @Override
     @Override
 }
